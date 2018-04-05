299330e6323e6215697876e9bec170bcb0beddbc
Alexandr Ropan <pretorian00757@gmail.com>
2018-04-05 13:12:26 +0300

    date

--- js/script.js
+++ js/script.js
@@ -2,11 +2,24 @@ var day = document.querySelector("#day")
 var gonight = document.querySelector("#night")
 var h1C
 	h1C = document.querySelector("#h1Change")
+gonight.setAttribute("disabled","false");
 
 day.onclick = function(){
 	h1C.innerHTML = "Сейчас ночь"
  	document.body.className = "night";
- 	document.onmousemove = function pony(){
+	day.setAttribute("disabled","false");
+	gonight.removeAttribute("disabled");
+
+}
+gonight.onclick = function(){
+	h1C.innerHTML = "Сейчас День"	
+ 	document.body.className = "day";
+ 	gonight.setAttribute("disabled","true");
+ 	day.removeAttribute("disabled");
+ 	
+}
+
+document.onmousemove = function pony(){
 	document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<img src="https://vignette.wikia.nocookie.net/flutterbutter/images/6/62/Twilight_Sparkle_Pixel_Dance.gif/revision/latest?cb=20140516115830" id="pony">');
 	var pony = document.getElementById('pony');
 	pony.style.position = 'fixed';
@@ -17,13 +30,27 @@ day.onclick = function(){
 	}
 
 }
+
+Data = new Date();
+Year = Data.getFullYear();
+Month = Data.getMonth();
+Day = Data.getDate();
+ 
+
+switch (Month)
+{
+  case 0: fMonth="января"; break;
+  case 1: fMonth="февраля"; break;
+  case 2: fMonth="марта"; break;
+  case 3: fMonth="апреля" + ""; break;
+  case 4: fMonth="мае"; break;
+  case 5: fMonth="июня"; break;
+  case 6: fMonth="июля"; break;
+  case 7: fMonth="августа"; break;
+  case 8: fMonth="сентября"; break;
+  case 9: fMonth="октября"; break;
+  case 10: fMonth="ноября"; break;
+  case 11: fMonth="декабря"; break;
 }
-gonight.onclick = function(){
-	h1C.innerHTML = "Сейчас День"
-	
- 	document.body.className = "day";
-}
-police.onclick = function(){	
- 	document.body.className = "what";
- 	
-}
+
+document.write("<br>"+ "<br>"+ "Сегодня "+Day+" "+fMonth+" "+Year+" года");
